3a8a171c4191061c6c2a52f5e3dafb45555fdad4
Keith Franklin <keith30895@gmail.com>
2018-01-26 20:29:02 +0530

    nav bar is functional

--- angular-files/src/app/component/nav-bar/nav-bar.component.ts
+++ angular-files/src/app/component/nav-bar/nav-bar.component.ts
@@ -7,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
   cd_headerStatus = false;
-  cd_selectedStatus = false;
+  
+  list=['Home','Projects','Blog','About','contact'];
+  cd_selectedStatus = [];
+  current=0;
   constructor() { }
 
   ngOnInit() {
+    this.cd_selectedStatus = this.list.map(el=>{
+      return false;
+    });
+    this.cd_selectedStatus[this.current] = true;
   }
-
+  calculate(){
+    // console.log(this.cd_selectedStatus);
+    let tempVal = this.current *200;
+    return tempVal+'px';
+  }
+  select(i){
+    // this.current = i;
+    if(this.current != i){
+      this.cd_selectedStatus[this.current] = false;
+      this.current = i;
+      this.cd_selectedStatus[this.current] = true;
+    }else{
+      this.cd_selectedStatus[this.current] = true;
+    }
+  }
 }
